0b4a91a1c434a6fdce7580e5ab7d8dca513c6e6f
CamilaMw <139786544+CamilaMw@users.noreply.github.com>
2024-11-13 05:31:24 -0300

    first commit

---
+++ Lista/src/lista/Lista.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lista;
+import java.util.List;
+import java.util.ArrayList;
+/**
+ *
+ * @author camila
+ */
+public class Lista {
+ 
+    public static void main(String[] args) {
+        List<String> giulia= new ArrayList<String>();
+        giulia.add("Afranio");
+        giulia.add("Leticia");
+        giulia.add("Pedro");
+        giulia.add("Ana");
+        giulia.remove(1);
+    }
+    
+}
